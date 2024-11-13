ad1dc4f3a7653b33fe94a9b5aec43ff4ab29b915
Abhishek ij <136907156+Abhishek2003ij@users.noreply.github.com>
2024-11-13 18:31:25 +0530

    Add files via upload

---
+++ Role-Based (2)/src/main/java/com/Auth/Role_Based/RoleBasedApplication.java
@@ -0,0 +1,13 @@
+package com.Auth.Role_Based;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedApplication.class, args);
+	}
+
+}
---
+++ Role-Based (2)/src/test/java/com/Auth/Role_Based/RoleBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Auth.Role_Based;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
